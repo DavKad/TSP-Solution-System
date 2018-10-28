ab335e7873513afbb6ffe2c7a3cdd8a4ada96a49
Dawid <dawidkadziolka@gmail.com>
2018-10-28 22:00:21 +0100

    Geocoding works (very dirty way)

--- TSP_System/src/main/java/controllers/MainController.java
+++ TSP_System/src/main/java/controllers/MainController.java
@@ -1,6 +1,13 @@
 package controllers;
 
 import dialogs.Dialog;
+import javafx.geometry.Pos;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
@@ -10,8 +17,9 @@ import javafx.scene.control.ButtonType;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import jdk.nashorn.internal.objects.annotations.Function;
+import properties.SearchProperties;
 import utils.UtilsConnection;
-
 
 import java.io.IOException;
 import java.net.URL;
@@ -19,10 +27,25 @@ import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
+
     @FXML
     public WebView webView;
 
+    @FXML
+    public TextField endField;
+
+    @FXML
+    public TextField fromField;
+
+    @FXML
+    public VBox addFieldContainer;
+
+    //Variables to load map page and properties management
+    private SearchProperties searchProperties = new SearchProperties();
+    private WebEngine webEngine;
+
     /*MenuBar methods*/
+    @Function
     public void closeApplication() {
         Optional<ButtonType> confirmationResult = Dialog.closeApplicationConfirmation();
         if (confirmationResult.get() == ButtonType.OK) {
@@ -30,6 +53,7 @@ public class MainController implements Initializable {
         }
     }
 
+    @Function
     public void openReport() {
         try {
             AnchorPane anchorPane = UtilsConnection.getFXML("/FXML/ReportWindow.fxml");
@@ -46,18 +70,93 @@ public class MainController implements Initializable {
 
     }
 
+    @Function
     public void openAbout() {
-        Dialog.aboutApplication();
+        Dialog.informationAlert(UtilsConnection.getBundles().getString("textAbout.app")
+                ,UtilsConnection.getBundles().getString("headerAbout.app")
+                ,UtilsConnection.getBundles().getString("aboutTitle.app"));
+    }
+
+    @Function
+    public void setLocation() {
+        String location = fromField.getText();
+        //Running JS function in very DIRTY WAY!!!!
+        try {
+            webEngine.executeScript(
+                    "geocoder.query('"+location+"', showMap);\n" +
+                            "function showMap(err, data) {\n" +
+                            "    if (data.lbounds) {\n" +
+                            "        map.fitBounds(data.lbounds);\n" +
+                            "    } else if (data.latlng) {\n" +
+                            "        map.setView([data.latlng[0], data.latlng[1]], 13);\n" +
+                            "        L.marker([data.latlng[0], data.latlng[1]]).addTo(map)" +
+                            "    }\n" +
+                            "}"
+            );
+        } catch (Exception e) {
+            Dialog.error(UtilsConnection.getBundles().getString("error.map"));
+        }
+    }
+
+    @Function
+    private void addNewWayPoint(TextField nextField) {
+            nextField.setOnKeyReleased(event -> {
+                if (event.getCode().equals(KeyCode.ENTER)) {
+                    //Add new field and image
+                    HBox addPointContainer = new HBox();
+                    ImageView plus = new ImageView("/images/plus.png");
+                    TextField addPoint = new TextField();
+                    addPoint.setPromptText(UtilsConnection.getBundles().getString("addPoint.search"));
+                    addPointContainer.setAlignment(Pos.CENTER);
+                    addPointContainer.setSpacing(5);
+                    addPointContainer.getChildren().add(plus);
+                    addPointContainer.getChildren().add(addPoint);
+                    addFieldContainer.setSpacing(5);
+                    addFieldContainer.getChildren().add(addPointContainer);
+
+                    String location = nextField.getText();
+                    //Running JS function in very DIRTY WAY!!!!
+                    try {
+                        webEngine.executeScript(
+                                        "geocoder.query('"+location+"', showMap);\n" +
+                                                "function showMap(err, data) {\n" +
+                                                "    if (data.lbounds) {\n" +
+                                                "        map.fitBounds(data.lbounds);\n" +
+                                                "    } else if (data.latlng) {\n" +
+                                                "        map.setView([data.latlng[0], data.latlng[1]]).setZoom(18);\n" +
+                                                "        L.marker([data.latlng[0], data.latlng[1]]).addTo(map)" +
+                                                "    }\n" +
+                                                "}"
+                        );
+                    } catch (Exception e) {
+                        Dialog.error(UtilsConnection.getBundles().getString("error.map"));
+                    }
+                    //TODO Run routing algorithm
+                    addNewWayPoint(addPoint);
+                }
+            });
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        //Loading map through HTML file
         try {
-            URL url = this.getClass().getResource("/html/map.html");
-            webView.getEngine().load(url.toString());
+            URL url = this.getClass().getResource("/html/anotherMap.html");
+            webEngine = webView.getEngine();
+            webEngine.setJavaScriptEnabled(true);
+            webEngine.load(url.toString());
         } catch (Exception e) {
-            Dialog.error(e.getMessage());
+            Dialog.error(UtilsConnection.getBundles().getString("error.map"));
+        } finally {
+            webEngine.reload();
         }
+
+        //Binding
+        endField.textProperty().bindBidirectional(searchProperties.getEndProperties());
+        fromField.textProperty().bindBidirectional(searchProperties.getFromProperties());
+        addNewWayPoint(endField);
     }
+
+
 }
 
