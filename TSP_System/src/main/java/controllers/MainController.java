59aa9b0ae0110501f124658c2322a641c9e8c213
Dawid <dawidkadziolka@gmail.com>
2018-10-29 22:05:28 +0100

    Map Gecoding added.

--- TSP_System/src/main/java/controllers/MainController.java
+++ TSP_System/src/main/java/controllers/MainController.java
@@ -1,12 +1,7 @@
 package controllers;
 
 import dialogs.Dialog;
-import javafx.geometry.Pos;
-import javafx.scene.control.TextField;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyCode;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.ListView;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.application.Platform;
@@ -18,7 +13,6 @@ import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import jdk.nashorn.internal.objects.annotations.Function;
-import properties.SearchProperties;
 import utils.UtilsConnection;
 
 import java.io.IOException;
@@ -32,16 +26,9 @@ public class MainController implements Initializable {
     public WebView webView;
 
     @FXML
-    public TextField endField;
+    private ListView<String> listOfInterest;
 
-    @FXML
-    public TextField fromField;
-
-    @FXML
-    public VBox addFieldContainer;
-
-    //Variables to load map page and properties management
-    private SearchProperties searchProperties = new SearchProperties();
+    //Variables to load map page
     private WebEngine webEngine;
 
     /*MenuBar methods*/
@@ -77,84 +64,19 @@ public class MainController implements Initializable {
                 ,UtilsConnection.getBundles().getString("aboutTitle.app"));
     }
 
-    @Function
-    public void setLocation() {
-        String location = fromField.getText();
-        //Running JS function in very DIRTY WAY!!!!
-        try {
-            webEngine.executeScript(
-                    "geocoder.query('"+location+"', showMap);\n" +
-                            "function showMap(err, data) {\n" +
-                            "    if (data.lbounds) {\n" +
-                            "        map.fitBounds(data.lbounds);\n" +
-                            "    } else if (data.latlng) {\n" +
-                            "        map.setView([data.latlng[0], data.latlng[1]], 13);\n" +
-                            "        L.marker([data.latlng[0], data.latlng[1]]).addTo(map)" +
-                            "    }\n" +
-                            "}"
-            );
-        } catch (Exception e) {
-            Dialog.error(UtilsConnection.getBundles().getString("error.map"));
-        }
-    }
-
-    @Function
-    private void addNewWayPoint(TextField nextField) {
-            nextField.setOnKeyReleased(event -> {
-                if (event.getCode().equals(KeyCode.ENTER)) {
-                    //Add new field and image
-                    HBox addPointContainer = new HBox();
-                    ImageView plus = new ImageView("/images/plus.png");
-                    TextField addPoint = new TextField();
-                    addPoint.setPromptText(UtilsConnection.getBundles().getString("addPoint.search"));
-                    addPointContainer.setAlignment(Pos.CENTER);
-                    addPointContainer.setSpacing(5);
-                    addPointContainer.getChildren().add(plus);
-                    addPointContainer.getChildren().add(addPoint);
-                    addFieldContainer.setSpacing(5);
-                    addFieldContainer.getChildren().add(addPointContainer);
-
-                    String location = nextField.getText();
-                    //Running JS function in very DIRTY WAY!!!!
-                    try {
-                        webEngine.executeScript(
-                                        "geocoder.query('"+location+"', showMap);\n" +
-                                                "function showMap(err, data) {\n" +
-                                                "    if (data.lbounds) {\n" +
-                                                "        map.fitBounds(data.lbounds);\n" +
-                                                "    } else if (data.latlng) {\n" +
-                                                "        map.setView([data.latlng[0], data.latlng[1]]).setZoom(18);\n" +
-                                                "        L.marker([data.latlng[0], data.latlng[1]]).addTo(map)" +
-                                                "    }\n" +
-                                                "}"
-                        );
-                    } catch (Exception e) {
-                        Dialog.error(UtilsConnection.getBundles().getString("error.map"));
-                    }
-                    //TODO Run routing algorithm
-                    addNewWayPoint(addPoint);
-                }
-            });
-    }
-
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         //Loading map through HTML file
         try {
-            URL url = this.getClass().getResource("/html/anotherMap.html");
+            URL url = this.getClass().getResource("/html/mainMap.html");
             webEngine = webView.getEngine();
             webEngine.setJavaScriptEnabled(true);
-//            webEngine.load(url.toString());
+            webEngine.load(url.toString());
         } catch (Exception e) {
             Dialog.error(UtilsConnection.getBundles().getString("error.map"));
         } finally {
             webEngine.reload();
         }
-
-        //Binding
-        endField.textProperty().bindBidirectional(searchProperties.getEndProperties());
-        fromField.textProperty().bindBidirectional(searchProperties.getFromProperties());
-        addNewWayPoint(endField);
     }
 
 
--- TSP_System/src/main/resources/javascript/setMarkerOnce.js
+++
@@ -1,9 +0,0 @@
-var marker;
-    map.on('click', function (e) {
-        if(marker != null){
-            map.removeLayer(marker);
-        }
-        var lat = e.latlng.lat;
-        var long = e.latlng.lng;
-        marker = L.marker([lat, long]).addTo(map);
-    });
